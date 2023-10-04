7063879fb3d53c5da27fc9008b3308914b254196
kayl33n <146216454+kayl33n@users.noreply.github.com>
2023-10-04 11:51:31 -0400

    Add files via upload

---
+++ users_numbers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  4 11:38:32 2023
+
+@author: kayc2
+"""
+
+def main () :
+    print ("This program adds two numbers.")
+
+number1 = input("Enter the first number")
+number2 = input("Enter the second number")
+total = number1 + number2
+print ("The total is " + total + ".")
+if __name__ == "__main__":
+    main()
+    
