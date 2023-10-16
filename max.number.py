1a5165980f0fcc6d2fe8fd0c30ecfd9b58bb8aba
kayl33n <146216454+kayl33n@users.noreply.github.com>
2023-10-16 12:26:48 -0400

    Add files via upload

---
+++ max.number.py
@@ -0,0 +1,11 @@
+def max (num1, num2) :
+        if num1 >= num2:
+            return num1
+        return num2
+def main () :
+    larger = max (5,1)
+    print (larger)
+    
+if __name__ == "__main__":
+    main()
+    main()
